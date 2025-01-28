3315f04faa07554e68e89a28ac45577cef910bc6
sanjay <sanjaybaidhya123@gmail.com>
2025-01-28 14:10:17 +0545

    test

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+const PORT = 3000;
 
 app.use((req, res, next) => {
   // Allow cross-origin requests
-  res.setHeader('Access-Control-Allow-Origin', 'https://other-domain.com'); // Replace with your domain
+  res.setHeader('Access-Control-Allow-Origin', 'https://cookie-second.onrender.com/'); // Replace with your domain
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html')); // Adjust to your file's location
 });
 
 app.get('/set-cookie', (req, res) => {
   res.cookie('crossDomainCookie', 'cookieValue', {
     httpOnly: true,
-    secure: true, // Use HTTPS
+    // secure: true, // Use HTTPS
     sameSite: 'None', // Allow cross-site requests
-    domain: '.example.com', // Set for your domain or subdomains
+    // domain: '.example.com', // Set for your domain or subdomains
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
   res.send('Cookie set');
